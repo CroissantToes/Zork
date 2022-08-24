c00b50cf1650487268add154697e5c457c9f9dce
CroissantToes <cteles20@gmail.com>
2022-08-24 15:12:20 -0400

    Zork 1.1 initial commit

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -7,6 +7,23 @@ namespace Zork
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
+
+            string inputString = Console.ReadLine().Trim().ToUpper();
+
+            Console.WriteLine(inputString);
+
+            if(inputString == "QUIT")
+            {
+                Console.WriteLine("Thank you for playing.");
+            }
+            else if (inputString == "LOOK")
+            {
+                Console.WriteLine("This is an open field west of a white house with a boarded front door.\n A rubber mat saying 'Welcome to Zork!' lies by the front door.");
+            }
+            else
+            {
+                Console.WriteLine($"Unknown Command: {inputString}");
+            }
         }
     }
 }
