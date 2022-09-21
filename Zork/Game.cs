2d6b90822fd4219fbfefc97f2fad344eabb82ca6
CroissantToes <cteles20@gmail.com>
2022-09-21 15:17:16 -0400

    Classwork 9/21

---
+++ Zork/Game.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zork
+{
+    internal class Game
+    {
+        public World World { get; set; }
+
+        public Player Player { get; set; }
+
+        public void Run()
+        {
+            InitializeRoomDescriptions();
+
+            Room previousRoom = null;
+
+            bool isRunning = true;
+
+            while (isRunning)
+            {
+                Console.Write($"{Player.currentRoom}\n> ");
+
+                if (previousRoom != Player.currentRoom && !Player.currentRoom.HasBeenVisited)
+                {
+                    Console.WriteLine(Player.currentRoom.Description);
+                    previousRoom = Player.currentRoom;
+                    Player.currentRoom.HasBeenVisited = true;
+                }
+
+                string inputString = Console.ReadLine().Trim();
+                Commands command = ToCommand(inputString);
+
+                string outputString;
+                switch (command)
+                {
+                    case Commands.Quit:
+                        outputString = "Thank you for playing!";
+                        isRunning = false;
+                        break;
+                    case Commands.Look:
+                        outputString = Player.currentRoom.Description;
+                        break;
+                    case Commands.North:
+                    case Commands.South:
+                    case Commands.East:
+                    case Commands.West:
+                        if (Player.Move(command))
+                        {
+                            outputString = $"You moved {command}.";
+                        }
+                        else
+                        {
+                            outputString = "The way is shut!";
+                        }
+                        break;
+                    default:
+                        outputString = "Unknown command.";
+                        break;
+                }
+
+                Console.WriteLine(outputString);
+            }
+        }
+
+        private void InitializeRoomDescriptions()
+        {
+            var roomMap = new Dictionary<string, Room>();
+
+            foreach (Room room in World.Rooms)
+            {
+                roomMap.Add(room.Name, room);
+            }
+
+            roomMap["Rocky Trail"].Description = "You are on a rock-strewn trail.";
+            roomMap["South of House"].Description = "You are facing the south side of a white house. There is no door here, and all thw windows are barred.";
+            roomMap["Canyon View"].Description = "You are at the top of the Great Canyon on its south wall.";
+
+            roomMap["Forest"].Description = "This is a forest, with trees in all directions around you.";
+            roomMap["West of House"].Description = "This is an open field west of a white house, with a boarded front door.";
+            roomMap["Behind House"].Description = "You are behind the white house. In one corner of the house, there is a small window which is slightly ajar.";
+
+            roomMap["Dense Woods"].Description = "This is a dimly lit forest, with large trees all around. To the east, there appears to be sunlight.";
+            roomMap["North of House"].Description = "You are facing the north side of a white house. There is no door here, and all the windows are barred.";
+            roomMap["Clearing"].Description = "You are in a clearing, with a forest surrounding you on the west and south.";
+        }
+
+        private static Commands ToCommand(string commandString)
+        {
+            return Enum.TryParse<Commands>(commandString, true, out Commands command) ? command : Commands.Unknown;
+        }
+    }
+}
